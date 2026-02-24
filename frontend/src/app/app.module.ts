969478b300ac90f3f0ceddd5db8bea0e0e5ecd86
Gopal7032 <gopalmarri80@gmail.com>
2026-02-24 11:39:33 +0530

    Add frontend and backend source code

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FeedComponent } from './feed/feed.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FeedComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
