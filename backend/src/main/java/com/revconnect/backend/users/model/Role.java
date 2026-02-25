0ed30e1a1c91ff0c6833b2890f5b5f252d2b90f7
Gopal7032 <gopalmarri80@gmail.com>
2026-02-25 13:05:02 +0530

    Revert "Added UI page for user profile"
    This reverts commit aaac791ca5939ee64e1c9bddb8deb1091d57e031.

--- backend/src/main/java/com/revconnect/backend/users/model/Role.java
+++
@@ -1,7 +0,0 @@
-package com.revconnect.backend.users.model;
-
-public enum Role {
-    PERSONAL,
-    CREATOR,
-    BUSINESS
-}
--- backend/src/main/java/com/revconnect/backend/users/repository/ProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.revconnect.backend.users.repository;
-
-import com.revconnect.backend.users.model.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileRepository extends JpaRepository<Profile, Long> {
-
-    Profile findByUserId(Long userId);
-}
